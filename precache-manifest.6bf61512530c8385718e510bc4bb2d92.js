9beea6fe7b0049741904433173fb5936bb130a48
0x04 <ok@0x04.de>
2020-04-16 15:13:27 +0200

    Updates

---
+++ precache-manifest.6bf61512530c8385718e510bc4bb2d92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "356c4cebab2d594dd9e909676790b36e",
+    "url": "/pseudo-cipherer/index.html"
+  },
+  {
+    "revision": "ba90650c2f43d64a6c35",
+    "url": "/pseudo-cipherer/static/css/main.749e82c1.chunk.css"
+  },
+  {
+    "revision": "3901443936e9dc626181",
+    "url": "/pseudo-cipherer/static/js/2.3d8885a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pseudo-cipherer/static/js/2.3d8885a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba90650c2f43d64a6c35",
+    "url": "/pseudo-cipherer/static/js/main.6eee2155.chunk.js"
+  },
+  {
+    "revision": "2fbdc37902fe2d93fe29",
+    "url": "/pseudo-cipherer/static/js/runtime-main.dec82f97.js"
+  }
+]);
